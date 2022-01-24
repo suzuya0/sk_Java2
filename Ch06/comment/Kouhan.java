729d1b1fb544722a9499e1830e1d2db52c940b70
suzuya0 <sakura240613@gmail.com>
2022-01-24 13:29:58 +0900

    [Finish]Ch6

---
+++ Ch06/comment/Kouhan.java
@@ -0,0 +1,13 @@
+package comment;
+
+public class Kouhan {
+  public static void callDeae(){
+    System.out.println("ええい、こしゃくな。くせ者だ！であえであえ");
+  }
+  public static void showMondokoro() throws Exception{
+    System.out.println("飛車さん、角さん。もういいでしょ");
+    System.out.println("この紋所が目に入らぬか");
+    Thread.sleep(3000);
+    Zenhan.doTogame();
+  }
+}
